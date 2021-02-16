807ade85b90b4c809489c2dfc05f8de75dabed27
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-02-16 13:13:06 +0100

    first commit

---
+++ Projecten2_TicketingPlatform/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecten2_TicketingPlatform.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
