e1db71610d13239bc30a78003575ce89cae52fee
Leon Paelinck <leon.paelinck@student.hogent.be>
2021-02-16 19:50:23 +0100

    DCD

---
+++ Projecten2_TicketingPlatform/Models/Gebruiker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_21_22
+{
+    public abstract class Gebruiker
+    {
+        private String _gebruikersnaam;
+        private int _wachtwoord;
+
+        public Status Status
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ Projecten2_TicketingPlatform/Models/Klant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_21_22
+{
+    public class Klant : Gebruiker
+    {
+    }
+}
