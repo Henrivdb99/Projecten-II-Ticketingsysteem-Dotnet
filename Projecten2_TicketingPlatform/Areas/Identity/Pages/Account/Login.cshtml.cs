3c549a05631ef6dae27ede6f799ce300b77164d3
Henri <henri.vandenberghe@student.hogent.be>
2021-02-18 18:00:22 +0100

    aanmeldpogingen loggen

--- Projecten2_TicketingPlatform/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Projecten2_TicketingPlatform/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -78,26 +78,21 @@ namespace Projecten2_TicketingPlatform.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation(DateTime.Now + _userManager.GetUserName(User) + " Gelukt");
                     return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
+                }                
                 if (result.IsLockedOut)
                 {
-                    _logger.LogWarning("User account locked out.");
+                    _logger.LogWarning(DateTime.Now + _userManager.GetUserName(User) + " Mislukt");
                     return RedirectToPage("./Lockout");
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    _logger.LogInformation(DateTime.Now + _userManager.GetUserName(User) + " Mislukt");
+                    ModelState.AddModelError(string.Empty, "Ongeldige inlogpoging.");
                     return Page();
                 }
             }
