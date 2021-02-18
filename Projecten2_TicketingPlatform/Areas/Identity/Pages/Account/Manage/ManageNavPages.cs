330be07fae308d3284a43a8f613f11d533af0e80
Henri Van Den Berghe <henri.vandenberghe@student.hogent.be>
2021-02-18 12:10:40 +0100

    external logins uit IF gehaald

--- Projecten2_TicketingPlatform/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Projecten2_TicketingPlatform/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -14,15 +14,11 @@ namespace Projecten2_TicketingPlatform.Areas.Identity.Pages.Account.Manage
 
         public static string ChangePassword => "ChangePassword";
 
-        public static string ExternalLogins => "ExternalLogins";
-
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
-
-        public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
 
         private static string PageNavClass(ViewContext viewContext, string page)
         {
