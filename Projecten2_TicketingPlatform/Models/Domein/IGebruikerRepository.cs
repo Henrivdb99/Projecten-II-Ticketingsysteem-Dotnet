9724c747d31d591fd1bdc8bdf419dfeec5775a22
Leon Paelinck <leon.paelinck@student.hogent.be>
2021-02-21 14:27:33 +0100

    kliene udmate

--- Projecten2_TicketingPlatform/Models/Domein/IGebruikerRepository.cs
+++ Projecten2_TicketingPlatform/Models/Domein/IGebruikerRepository.cs
@@ -8,5 +8,6 @@ namespace Projecten2_TicketingPlatform.Models.Domein
     public interface IGebruikerRepository
     {
         Gebruiker GetBy(String gebruikersnaam);
+        void SaveChanges();
     }
 }
