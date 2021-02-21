ab3de484cd847dce7bfe6a7752b369f0fd02053a
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-02-21 15:24:51 +0100

    Type Klant naar type String aangepast voor klantId

--- Projecten2_TicketingPlatform/Models/Domein/Ticket.cs
+++ Projecten2_TicketingPlatform/Models/Domein/Ticket.cs
@@ -13,12 +13,12 @@ namespace Projecten2_TicketingPlatform.Models.Domein
         public DateTime DatumAanmaken { get; set; }
         public string Omschrijving { get; set; }
         public int TypeTicket { get; set; }
-        public Klant Klant { get; set; }
+        public string Klant { get; set; }
         public string Technieker { get; set; }
         public string[] Opmerkingen { get; set; }
         public string Bijlage { get; set; }
 
-        public Ticket(string titel, int ticketId, TicketStatus ticketStatus, DateTime date, string omschrijving, int typeTicket, Klant klant, string technieker, string[] opmerkingen, string bijlage)
+        public Ticket(string titel, int ticketId, TicketStatus ticketStatus, DateTime date, string omschrijving, int typeTicket, string klantId, string technieker, string[] opmerkingen, string bijlage)
         {
             Titel = titel;
             Ticketid = ticketId;
@@ -26,7 +26,7 @@ namespace Projecten2_TicketingPlatform.Models.Domein
             DatumAanmaken = date;
             Omschrijving = omschrijving;
             TypeTicket = typeTicket;
-            Klant = klant;
+            Klant = klantId;
             Technieker = technieker;
             Opmerkingen = opmerkingen;
             Bijlage = bijlage;
