a38a29c5c2dcc343aed8d20c7bb895021d62d243
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-02-21 14:31:19 +0100

    ITicketRepository added

---
+++ Projecten2_TicketingPlatform/Models/Domein/ITicketRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projecten2_TicketingPlatform.Models.Domein
+{
+    public interface ITicketRepository
+    {
+        Ticket GetById(int ticketId);
+        IEnumerable<Ticket> GetAllByClientId(int klantId);
+        void Add(Ticket ticket);
+        void Delete(Ticket ticket);
+        void SaveChanges();
+    }
+}
