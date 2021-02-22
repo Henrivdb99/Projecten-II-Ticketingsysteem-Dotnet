baeb335705e6d5b8fedf55a121bf9a5e58fc75f1
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-02-22 19:29:57 +0100

    DummyApplicationDbContext aangemaakt + ticket

---
+++ Projecten2_TicketingPlatform.Tests/Data/DummyApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Projecten2_TicketingPlatform.Models.Domein;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecten2_TicketingPlatform.Tests.Data
+{
+    public class DummyApplicationDbContext
+    {
+        public Ticket Ticket { get; }
+        public DummyApplicationDbContext()
+        {
+            Ticket= new Ticket("Ticket20", 1, TicketStatus.Aangemaakt, DateTime.Today, "Ik heb een probleem", 1, "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", "Jan de technieker", null, null);
+        }
+    }
+}
