1817dc2b8a044a9fa73094ba7dbe3b84a90108e2
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-02-22 15:45:10 +0100

    TicketRepository added, not implemented

---
+++ Projecten2_TicketingPlatform/Data/Repositories/TicketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Projecten2_TicketingPlatform.Models.Domein;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projecten2_TicketingPlatform.Data.Repositories
+{
+    public class TicketRepository : ITicketRepository
+    {
+        public void Add(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetAllByClientId(int klantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket GetById(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
