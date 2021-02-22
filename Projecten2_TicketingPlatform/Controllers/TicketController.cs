872a61fa5fbb6f8cf6f9acf364de8e2f68ff876a
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-02-22 19:31:35 +0100

    NotImplemented TicketController Added

--- Projecten2_TicketingPlatform/Controllers/TicketController.cs
+++ Projecten2_TicketingPlatform/Controllers/TicketController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Projecten2_TicketingPlatform.Models.Domein;
+using Projecten2_TicketingPlatform.Models.TicketViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,41 @@ namespace Projecten2_TicketingPlatform.Controllers
 {
     public class TicketController : Controller
     {
+        private ITicketRepository _ticketRepository;
+
+        public TicketController(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
         public IActionResult Index()
         {
             IEnumerable<Ticket> tickets = null;
             return View(tickets);
             throw new NotImplementedException();
         }
+        #region == Create Methodes ==
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public IActionResult Create(EditViewModel ticketVm)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region == Edit Methodes ==
+        public IActionResult Edit(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public IActionResult Edit(int ticketId, EditViewModel editViewModel)
+        {
+            throw new NotImplementedException();
+        } 
+        #endregion
     }
 }
