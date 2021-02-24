df24391c1080bff7f23d301e7cb461113a0a34c1
Henri <henri.vandenberghe@student.hogent.be>
2021-02-24 11:44:02 +0100

    verschillende rollen toegevoegd + bijhorend dashbord

--- Projecten2_TicketingPlatform/Data/TicketingPlatformDataInitializer.cs
+++ Projecten2_TicketingPlatform/Data/TicketingPlatformDataInitializer.cs
@@ -30,10 +30,25 @@ namespace Projecten2_TicketingPlatform.Data
 
         private async Task InitializeUsers()
         {
-            string eMailAddress = "klant@hogent.be";
-            IdentityUser user = new IdentityUser { UserName = eMailAddress, Email = eMailAddress };
-            await _userManager.CreateAsync(user, "P@ssword1");
-            await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, "klant"));
+            string eMailAddress1 = "klant@hogent.be";
+            IdentityUser user1 = new IdentityUser { UserName = eMailAddress1, Email = eMailAddress1 };
+            await _userManager.CreateAsync(user1, "P@ssword1");
+            await _userManager.AddClaimAsync(user1, new Claim(ClaimTypes.Role, "klant"));
+
+            string eMailAddress2 = "technieker@hogent.be";
+            IdentityUser user2 = new IdentityUser { UserName = eMailAddress2, Email = eMailAddress2 };
+            await _userManager.CreateAsync(user2, "P@ssword1");
+            await _userManager.AddClaimAsync(user2, new Claim(ClaimTypes.Role, "technieker"));
+
+            string eMailAddress3 = "sm@hogent.be";
+            IdentityUser user3 = new IdentityUser { UserName = eMailAddress3, Email = eMailAddress3 };
+            await _userManager.CreateAsync(user3, "P@ssword1");
+            await _userManager.AddClaimAsync(user3, new Claim(ClaimTypes.Role, "supportmanager"));
+
+            string eMailAddress4 = "admin@hogent.be";
+            IdentityUser user4 = new IdentityUser { UserName = eMailAddress4, Email = eMailAddress4 };
+            await _userManager.CreateAsync(user4, "P@ssword1");
+            await _userManager.AddClaimAsync(user4, new Claim(ClaimTypes.Role, "administrator"));
         }
     }
 }
