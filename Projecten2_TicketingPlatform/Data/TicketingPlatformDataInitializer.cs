5753cc673e38a77926ed8c0296a3816aea0b96db
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-03-09 14:36:24 +0100

    Data aangepast

--- Projecten2_TicketingPlatform/Data/TicketingPlatformDataInitializer.cs
+++ Projecten2_TicketingPlatform/Data/TicketingPlatformDataInitializer.cs
@@ -24,15 +24,18 @@ namespace Projecten2_TicketingPlatform.Data
             if (_dbcontext.Database.EnsureCreated())
             {
                 await InitializeUsers();
-
+                
                 ////Nog aan te vullen verdere data later in project.
                 var tickets = new List<Ticket>
             {
-                     new Ticket("TitelTicket4", TicketStatus.Geannuleerd, DateTime.Today.AddDays(-4), "Hoe maak ik een ticket aan?", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Jef de Technieker"),
-                     new Ticket("TitelTicket1", TicketStatus.Aangemaakt, DateTime.Today.AddDays(-3), "Blijkbaar heb ik een probleem", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Jef de Technieker"),
-                     new Ticket("TitelTicket2", TicketStatus.Aangemaakt, DateTime.Today.AddDays(-2), "Nog steeds hetzelfde probleem", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Jef de Technieker"),
-                     new Ticket("TitelTicket3", TicketStatus.Aangemaakt, DateTime.Today.AddDays(-1), "Het gaat niet weg", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Bram de Technieker"),
-                     new Ticket("TitelTicket4", TicketStatus.Aangemaakt, DateTime.Today, "Zijn jullie daar?", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Jef de Technieker")
+                     new Ticket("2020-Error 2980", TicketStatus.Afgehandeld, DateTime.Today.AddMonths(-4), "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Colin C. Watkins"),
+                     new Ticket("2020-Error 109271", TicketStatus.Afgehandeld, DateTime.Today.AddMonths(-2), "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Colin C. Watkins"),
+
+                     new Ticket("2020-Authorisatie Probleem", TicketStatus.Geannuleerd, DateTime.Today.AddDays(-4), "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Colin C. Watkins"),
+                     new Ticket("2020-Error 5038", TicketStatus.Aangemaakt, DateTime.Today.AddDays(-3), "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Patrick D. Wimbush"),
+                     new Ticket("2020-Foutcode 966", TicketStatus.Aangemaakt, DateTime.Today.AddDays(-2),"Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Jef de Technieker"),
+                     new Ticket("2020-Foutcode 409 bij validatie", TicketStatus.InBehandeling, DateTime.Today.AddDays(-1), "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Colin C. Watkins"),
+                     new Ticket("2020-Error 5200", TicketStatus.InBehandeling, DateTime.Today, "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.", "1", "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", "Patrick D. Wimbush")
             };
 
                 _dbcontext.Tickets.AddRange(tickets);
@@ -41,11 +44,11 @@ namespace Projecten2_TicketingPlatform.Data
                 {
                     new Contract(DateTime.Today.AddMonths(-13), "type 1", 1, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105",ContractStatus.Afgelopen),
                     new Contract( DateTime.Today.AddMonths(-8), "type 2", 1, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", ContractStatus.InBehandeling),
-                    new Contract(DateTime.Today.AddMonths(-1), "type 1", 2, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105"),
-                    new Contract(DateTime.Today.AddMonths(-26), "type 1", 2, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", ContractStatus.Afgelopen),
+                    new Contract(DateTime.Today.AddMonths(-26), "type 1", 2, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105",ContractStatus.Afgelopen),
+                    new Contract(DateTime.Today.AddMonths(-28), "type 1", 2, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", ContractStatus.Afgelopen),
                     new Contract(DateTime.Today.AddMonths(-3), "type 1", 2, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105", ContractStatus.Actief),
-                    new Contract(DateTime.Today, "type 4", 3, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105"),
-                    new Contract(DateTime.Today.AddMonths(-4), "type 3", 3, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105")
+                    new Contract(DateTime.Today.AddYears(-4), "type 4", 3, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105",ContractStatus.Afgelopen),
+                    new Contract(DateTime.Today.AddMonths(-4), "type 3", 3, "210771fc - 21f2 - 47e4 - a902 - 986e2d199105",ContractStatus.NietActief)
                 };
                 _dbcontext.Contracten.AddRange(contractenVoorKlant);
 
