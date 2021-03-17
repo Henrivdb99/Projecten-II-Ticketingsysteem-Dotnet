bf831a6777501f48aa45d8a6fe176485f633ace1
Korneel Meyers <korneel.meyers@student.hogent.be>
2021-03-17 13:54:06 +0100

    Added contracttypes to dummy

--- Projecten2_TicketingPlatform.Tests/Data/DummyApplicationDbContext.cs
+++ Projecten2_TicketingPlatform.Tests/Data/DummyApplicationDbContext.cs
@@ -11,13 +11,18 @@ namespace Projecten2_TicketingPlatform.Tests.Data
         public Contract ContractActief { get; }
         public Contract ContractNietActief { get; }
         public Contract ContractAfgelopen { get; }
+        public ContractType Contract24_7 { get; set; }
+        public ContractType ContractWerkuren { get; set; }
 
         public DummyApplicationDbContext()
         {
-            Ticket= new Ticket("Ticket20", TicketStatus.Aangemaakt, DateTime.Today, "Ik heb een probleem", "1", "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", "Jan de technieker", null, null);
-            ContractActief = new Contract(DateTime.Today,"1", 2, "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", ContractEnContractTypeStatus.Actief);
-            ContractNietActief = new Contract(DateTime.Today.AddDays(400), "1", 1, "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", ContractEnContractTypeStatus.NietActief);
-            ContractAfgelopen = new Contract(DateTime.Today.AddDays(-300), "1", 1, "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", ContractEnContractTypeStatus.Afgelopen);
+            Contract24_7 = new ContractType("Contract24/7", ContractEnContractTypeStatus.Actief, ManierVanAanmakenTicket.Applicatie, TijdstipTicketAanmaken.Altijd, 1, 10, 100);
+            ContractWerkuren = new ContractType("ContractWerkuren", ContractEnContractTypeStatus.Actief, ManierVanAanmakenTicket.Applicatie, TijdstipTicketAanmaken.TijdensWerkdagen, 1, 10, 100);
+
+            Ticket = new Ticket("Ticket20", TicketStatus.Aangemaakt, DateTime.Today, "Ik heb een probleem", "1", "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", "Jan de technieker", null, null);
+            ContractActief = new Contract(DateTime.Today, Contract24_7, 2, "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", ContractEnContractTypeStatus.Actief);
+            ContractNietActief = new Contract(DateTime.Today.AddDays(400), Contract24_7, 1, "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", ContractEnContractTypeStatus.NietActief);
+            ContractAfgelopen = new Contract(DateTime.Today.AddDays(-300), Contract24_7, 1, "bff6a934 - 0dca - 4965 - b9fc - 91c3290792c8", ContractEnContractTypeStatus.Afgelopen);
         }
     }
 }
