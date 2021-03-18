f1f62ccf62ee970ab42ad7b1dc701acecf3aab5c
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-03-18 09:38:30 +0100

    Databanken deel 1 (werkt nog niet)

--- Projecten2_TicketingPlatform/Startup.cs
+++ Projecten2_TicketingPlatform/Startup.cs
@@ -34,7 +34,7 @@ namespace Projecten2_TicketingPlatform
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("TickentingPlatformContext")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
                        
